7fa24e51a69582c4829868203871249b41bf0a2b
Luis Amorim <luis@bizut.com.br>
2021-06-28 12:49:48 -0300

    removendo pasta

--- app/public/wp-content/themes/yootheme/cache/theme-5921cec0.php
+++
@@ -1,9 +0,0 @@
-<?php // $file = C:/Users/luisf/Local Sites/next/app/public/wp-content/themes/yootheme/vendor/yootheme/styler/config/theme.json
-
-return [
-  'defaults' => [
-    'less' => [], 
-    'custom_less' => '', 
-    'style' => 'fuse'
-  ]
-];
